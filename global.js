685082d19587fbd12dc59d743d80c9f818538b21
Aquets <37075016+Aquets@users.noreply.github.com>
2020-03-16 15:08:46 +0100

    menu

--- global.js
+++ global.js
@@ -4,12 +4,13 @@ $(function(){
 });
 
 function menuClick() {
-  var x = $("#menu").position();
 
-  console.log(x);
-  $("#menu").css("left", x.left - $("#menu").width());
-
-  if($("#menu").css('left') !== 100%){
-    $("#menu").css("left", '100%');
+  console.log($("#menu").css('right'));
+  var right = $("#menu").css('right');
+  if (right != '0px') {
+    $("#menu").css("right", 0);
+  }else {
+    $("#menu").css("right", -$("#menu").width());
   }
+
 }
