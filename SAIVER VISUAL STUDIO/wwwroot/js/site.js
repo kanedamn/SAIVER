cbe735a429294272198949f5f4cf2f591f47d155
Martina <martinamelillo97@gmail.com>
2020-03-16 21:26:53 +0100

    VISUAL STUDIO

---
+++ SAIVER VISUAL STUDIO/wwwroot/js/site.js
@@ -0,0 +1,21 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your Javascript code.
+
+$(function () {
+    $('#menuButton').click(menuClick)
+});
+
+var menuDisplay = false;
+function menuClick() {
+
+    console.log($("#menu").css("width"));
+    if (menuDisplay == false) {
+        $("#menu").css("right", 0);
+    } else {
+        $("#menu").css("right", '-34vw');
+    }
+    menuDisplay = !menuDisplay;
+
+}
